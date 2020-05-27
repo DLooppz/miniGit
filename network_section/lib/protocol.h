23a3c9eb5fda1a2827f50c84b2b4e3b6d376ee73
Matias Kloster <klostermts@gmail.com>
2020-05-27 10:03:12 -0300

    signin, signout, signup and help commands finished

--- network_section/lib/protocol.h
+++ network_section/lib/protocol.h
@@ -3,9 +3,12 @@
 
 #define SERVERPORT 7000
 #define BUFFSIZE 512
+#define COMMANDLEN 64
 #define USERLEN 64
 #define PASSLEN 64
 #define USERDIR "users/"
+#define HELPDIR "lib/help.txt"
+#define USERFMT ".txt"
 
 #include<stdlib.h>
 #include<stdio.h>
@@ -15,7 +18,9 @@
 #include<errno.h>
 #include<stdbool.h>
 #include<string.h>
-#include <dirent.h>
+#include<dirent.h>
+#include<sys/stat.h>
+#include<termios.h>
 
 // Declaration of variables, structs, enums, ...
 typedef struct {
@@ -23,14 +28,19 @@ typedef struct {
     struct sockaddr_in sock_addr;
     char username[USERLEN];
     char password[USERLEN];
-    bool loggedIn;
+    bool signedIn;
 } clientInfo_t;
 
-enum Command {
-    login,
-    logout,
+enum Command {  // Attention: if this is modified, function typed2enum should also be modified
+    signin,
+    signout,
+    signup,
     response,
-    file
+    file,
+    block,
+    push,       // no packets with this command, but its useful for using a switch in client.c
+    pull,       // no packets with this command, but its useful for using a switch in client.c
+    help        // no packets with this command, but its useful for using a switch in client.c
 };
 
 enum ResponseValues {
@@ -44,7 +54,7 @@ struct __attribute__((packed)) Header{
     uint16_t length;    // Lenght in bytes of complete packet
 };
 
-struct __attribute__((packed)) LoginArgs{
+struct __attribute__((packed)) SignArgs{
     char username[USERLEN];
     char password[PASSLEN];
 };
@@ -66,7 +76,7 @@ struct __attribute__((packed)) BlockArgs{
 struct __attribute__((packed)) Packet{
     struct Header header;
     union Payload {
-        struct LoginArgs loginArgs;
+        struct SignArgs signArgs; // Used for signin or signup
         struct ResponseArgs responseArgs;
         struct FileArgs fileArgs;
         struct BlockArgs blockArgs;
@@ -78,18 +88,23 @@ void error(const char *msg);
 
 int sendPacket(int socket, const struct Packet *packet);
 int recvPacket(int socket, struct Packet *packet);
-void setLoginPacket(struct Packet *packet, const char *user, const char *pass);
+void setSignUpPacket(struct Packet *packet, const char *user, const char *pass);
+void setSignInPacket(struct Packet *packet, const char *user, const char *pass);
 void setResponsePacket(struct Packet *packet, enum ResponseValues responseValue);
+void setSignOutPacket(struct Packet *packet);
 enum Command getPacketCommand(struct Packet *packet);
 enum ResponseValues getPacketResponseVal(struct Packet *packet);
 
 void setClientUser(clientInfo_t *clientInfo, char *username);
 void setClientPass(clientInfo_t *clientInfo, char *password);
-void setClientLoggedIn(clientInfo_t *clientInfo, bool loggedIn);
-bool isLoggedIn(clientInfo_t *clientInfo);
+void setClientSignedIn(clientInfo_t *clientInfo, bool signedIn);
+bool isSignedIn(clientInfo_t *clientInfo);
 int isRegistered(clientInfo_t *clientInfo);
 int isPassCorrect(clientInfo_t *clientInfo);
 
 int readNthLineFromFile(const char *srcPath, char *dest, int nthLine);
-
+void getStdInput(char *dest, uint maxLength, clientInfo_t *clientInfo, const char * msg);
+int createUser(clientInfo_t *clientInfo);
+enum Command typed2enum(char *typedInCommand);
+void printFile(const char *filePath);
 #endif
